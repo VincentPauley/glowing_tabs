b46980f5fcb87f676c78043f87a6bfa5fc2a69d8
Vincent Pauley <vinniepauley@gmail.com>
2016-01-20 21:38:18 -0600

    in progress, still nees color work and possible fade stuff

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-$("#trace").on('click', function() {
-  $(this).slideUp();
+
+/* handles switching of the selected class based on click */
+$("#tabWrapper").find("input[type=radio]").on("click", function() {
+  $(this).parents().addClass("selected").removeClass("hovering").siblings().removeClass("selected");
+});
+
+/* controls label hovers */
+$("#tabWrapper > label")
+.on("mouseenter", function() {
+    if($(this).hasClass("selected")) {
+
+    } else {
+      $(this).addClass("hovering");
+    }
+})
+.on("mouseleave", function() {
+  $(this).removeClass("hovering");
 });
