5dbc410c2fc4305079f55d2836e615bf0ed80184
Vincent P <vinniepauley@gmail.com>
2016-02-11 21:06:35 -0600

    close but needs classes

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-$("#trace").on('click', function() {
-  $(this).slideUp();
+var thisRadio;
+
+$('#glowTabs > label').on('mouseover', function() {
+	thisRadio = $(this).children('input[type=radio]');
+  if(thisRadio.is(':checked')) {
+
+  } else {
+  		// background color
+      $(this).css('background-color', '#ebebeb');
+      // shift text right
+      $(this).children('span').css('margin-left', '14px');
+  }
+});
+$('#glowTabs > label').on('mouseout', function() {
+	thisRadio = $(this).children('input[type=radio]');
+  if(thisRadio.is(':checked')) {
+
+  } else {
+  		// background color
+      $(this).css('background-color', '#303030');
+      // move text back
+      $(this).children('span').css('margin-left', '0px');
+  }
+});
+
+$('#glowTabs > label > input[type=radio]').on('click', function() {
+  $(this).parent('label').css('background-color', 'white').siblings().css('background-color', '#303030');
 });
